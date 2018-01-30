82a43fdc459f9bbed0460a06a58d307bdd021b0c
Stefan Larsson <stefan@larsson.nu>
2018-01-30 22:55:23 +0100

    Added mean and covariance prediction

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -192,7 +192,59 @@ void UKF::Prediction(double delta_t) {
   }
 
 
+  //
+  // PREDICT MEAN AND COVARIANCE
+  //
+
+  // From "Lesson 7: 24. Predicted mean and covariance assignment 2"
+
+  //create vector for weights
+  VectorXd weights = VectorXd(2*n_aug+1);
+
+  //create vector for predicted state
+  VectorXd x = VectorXd(n_x);
+
+  //create covariance matrix for prediction
+  MatrixXd P = MatrixXd(n_x, n_x);
+
+  // set weights
+  double weight_0 = lambda/(lambda+n_aug);
+  weights(0) = weight_0;
+  for (int i=1; i<2*n_aug+1; i++) {  //2n+1 weights
+    double weight = 0.5/(n_aug+lambda);
+    weights(i) = weight;
+  }
+
+  //predicted state mean
+  x.fill(0.0);
+  for (int i = 0; i < 2 * n_aug + 1; i++) {  //iterate over sigma points
+    x = x+ weights(i) * Xsig_pred.col(i);
+  }
+
+  //predicted state covariance matrix
+  P.fill(0.0);
+  for (int i = 0; i < 2 * n_aug + 1; i++) {  //iterate over sigma points
+
+    // state difference
+    VectorXd x_diff = Xsig_pred.col(i) - x;
+    //angle normalization
+    while (x_diff(3)> M_PI) x_diff(3)-=2.*M_PI;
+    while (x_diff(3)<-M_PI) x_diff(3)+=2.*M_PI;
+
+    // TODO change to P += to avoid IDE warning
+    P = P + weights(i) * x_diff * x_diff.transpose() ;
+  }
+
+
+
+  //
+  // ASSIGN OUTPUTS
+  //
+
   Xsig_pred_ = Xsig_pred;
+  x_ = x;
+  P_ = P;
+
 }
 
 /**
